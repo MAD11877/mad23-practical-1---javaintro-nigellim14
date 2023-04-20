ec9ccafcf7ed2a3e35046aef30b7e5b20a2ac6b3
Lim Shiwu Nigel <75166174+nigellim14@users.noreply.github.com>
2023-04-20 13:48:55 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -8,7 +8,7 @@ public class Question4
         int a, b;
         for(a = n; a <= n; a--) {
             for(b = 0; b <= a; b++) {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
