4ccca622c63504147052665f93c1568d20ac8583
Lim Shiwu Nigel <75166174+nigellim14@users.noreply.github.com>
2023-04-20 13:42:27 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -9,9 +9,6 @@ public class Question4
         int a, b;
         for(a = n; a <= n; a--) {
             for(b = 0; b <= a; b++) {
-                System.out.print("* ");
             }
-            System.out.println();
-    
   }
 }
