e2d6245eb7f1884b9737fd8e84157d8fd6af5a6c
Lim Shiwu Nigel <75166174+nigellim14@users.noreply.github.com>
2023-04-20 10:42:41 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,7 +1,8 @@
 public class Question1
 {
-  public static void main(String[] args)
-  {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+    
+    System.out.println("Hello World!");
+    
   }
 }
