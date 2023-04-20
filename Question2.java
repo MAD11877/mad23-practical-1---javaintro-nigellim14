a54bbbe6692d58f3f3528fa420d63e5339a8ab3d
Lim Shiwu Nigel <75166174+nigellim14@users.noreply.github.com>
2023-04-20 11:12:54 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,13 +5,12 @@ public class Question2
   public static void main(String[] args)
   {
    Scanner sc= new Scanner(System.in); 
-  //System.out.print("Enter your height ");
-  double height= sc.nextDouble();  
-    
-  //System.out.print("Enter your weight ");
-  double weight= sc.nextDouble();
 
-  double bmi = (100 * 100 * weight) / (height * height);  
-  System.out.println(bmi);  
+   double height= sc.nextDouble();  
+    
+   double weight= sc.nextDouble();
+
+   double bmi = (100 * 100 * weight) / (height * height);  
+   System.out.println(bmi);  
   }
 }
